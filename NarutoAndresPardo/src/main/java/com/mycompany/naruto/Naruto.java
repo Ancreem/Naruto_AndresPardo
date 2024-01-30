7615de6fc75ff41e29b3e105c559211876344b97
Ancreem <ancreemroyal@gmail.com>
2024-01-30 18:25:18 -0500

    docs: :memo: Se agregaron los ejecutores para los metodos hechos en clase

--- NarutoAndresPardo/src/main/java/com/mycompany/naruto/Naruto.java
+++ NarutoAndresPardo/src/main/java/com/mycompany/naruto/Naruto.java
@@ -10,17 +10,17 @@ public class Naruto {
 ////          MisionNinja Guardar
 //        MisionNinja misionNinja = new MisionNinja(9, 1, 1, LocalDate.of(2024,02,14),LocalDate.of(2024,02,16), "Completado");
 //        MisionNinjaController.saveMisions(misionNinja);
-
+//
 ////          ConsultasCompletado o Disponible
 //        System.out.println(MisionNinjaController.consultMisionsCompleteNinja(2));
 //        System.out.println(MisionNinjaController.consultMisionsDisponibleNinja(3));
-          
-
+//          
+//
 ////          MisionNinja Actualizar
-//        MisionNinja misionNinja = new MisionNinja();
+//        MisionNinja misionNinjaUpdate = new MisionNinja();
 //        misionNinja.setMisionNinjaId(3);
-//        MisionNinjaController.actualizarCompletadoId(misionNinja);
-//        MisionNinjaController.actualizarDisponibleId(misionNinja);
+//        MisionNinjaController.actualizarCompletadoId(misionNinjaUpdate);
+//        MisionNinjaController.actualizarDisponibleId(misionNinjaUpdate);
 
 
 
